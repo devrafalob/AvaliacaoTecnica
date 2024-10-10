d7ad4363654b53fc9ba5099a9238d660ddc09ccd
Rafael Lima Lobão <rafael.lobao104@academico.ifs.edu.br>
2024-10-10 15:21:09 -0300

    Terceira questão

---
+++ src/Questao3.java
@@ -0,0 +1,10 @@
+public class Questao3 {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        for(int k=1; indice>k; k++){
+            soma = soma+k;
+        }
+        System.out.println("Soma = " + soma);
+    }
+}
